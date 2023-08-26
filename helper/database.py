055720817397066369fccf57060b9550ac2fb7a7
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2023-08-27 03:43:22 +0700

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo
 import os
 from helper.date import add_date
-DB_NAME = os.environ.get("DB_NAME", "")
-DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "xflskay15")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://xflskay15:xflskay15@cluster0.sjgrnng.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
